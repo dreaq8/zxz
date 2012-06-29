a915aa92e2971844514c8c1a470dd81c4ea7236d
flavius <dreaq8@yahoo.co.uk>
2012-06-29 15:18:09 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zxz::Application.config.secret_token = 'b83d556c2cb03c57f4d9e442739cf1fdaa5bd84fdefa09c35e048a1762e1212e545dce2b7b32daccda265d65edf63c0fbdd7a37c569ce1f5596844318765d440'
